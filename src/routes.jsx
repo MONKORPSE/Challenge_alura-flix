3d501897ce28c17b6530e5664a49f1762759154b
Monika Baez <you@example.com>
2025-01-22 03:50:55 -0300

    Actualización

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import BasePage from "./pages/BasePage/BasePage";
-import Home from "./Pages/Home/Home";
-import NewVideo from "./Pages/NewVideo/NewVideo";
-import NotFound from "./Pages/NotFound/NotFound"; // Ruta corregida
+import Home from "./pages/Home/Home";
+import NewVideo from "./pages/NewVideo/NewVideo";
+import NotFound from "./pages/NotFound/NotFound"; // Ruta corregida
 
 const AppRoutes = () => {
   return (

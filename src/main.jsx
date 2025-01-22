93de890dd7da8076c8b1d1b2078251a1c84b7c51
Monika Baez <you@example.com>
2025-01-21 22:09:15 -0300

    Actualización

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import AppRoutes from "./routes";
+import GlobalStyles from "./Styles/GlobalStyles";
+import theme from "./Styles/Theme";
+import { VideoProvider } from "./Contexts/VideoContext";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <VideoProvider>
+          <GlobalStyles />
+          <AppRoutes />
+        </VideoProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ src/routes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import BasePage from "./Pages/BasePage/BasePage";
+import Home from "./Pages/Home/Home";
+import NewVideo from "./Pages/NewVideo/NewVideo";
+import NotFound from "./Pages/NotFound/NotFound"; // Ruta corregida
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<BasePage />}>
+        <Route index element={<Home />} />
+        <Route path="new-video" element={<NewVideo />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
